39cabab00ddab5839767c208393cf6aaaa2e769b
Lars Trieloff <trieloff@adobe.com>
2020-05-15 12:25:11 +0000

    docs(cache): add JSDocs
    fixes #10

--- src/cache.js
+++ src/cache.js
@@ -14,6 +14,22 @@ const QuickLRU = require('quick-lru');
 
 let lru = new QuickLRU({ maxSize: 1000 });
 
+/**
+ * Returns a memoized version of the function `fn`.
+ * @param {function} fn a function to memoize
+ * @param {function} hash a hash function to build a cache key.
+ * The hash function will be called with the `fn` and a list of
+ * it's arguments. A good hashing function will discard irrelevant
+ * arguments and join the resulting arguments
+ * @param {function} cacheresult a predicate function that will be called
+ * with the result of the function execution. Only when this function returns
+ * `true` will the result be cached. The default is that all results are
+ * cached.
+ * @param {function} cacheerror a predicate function that will be called
+ * with the error that the function execution throws. Only when this
+ * function returns `true` will the error be cached. The default is that
+ * errors are never cached, so that each new invocation will be uncached.
+ */
 function cache(fn, {
   hash = (...args) => args.join(),
   cacheresult = () => true,
