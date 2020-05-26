f7aa29bc4a8b08d55d9f581e67bae23b352c62b2
Lars Trieloff <trieloff@adobe.com>
2020-05-26 08:18:38 +0000

    docs(cache): add JSDoc to cache function

--- src/cache.js
+++ src/cache.js
@@ -17,24 +17,26 @@ let lru = new QuickLRU({ maxSize: 1000 });
 /**
  * Returns a memoized version of the function `fn`.
  * @param {function} fn a function to memoize
- * @param {function} hash a hash function to build a cache key.
+ * @param {object} opts caching options
+ * @param {function} opts.hash a hash function to build a cache key.
  * The hash function will be called with the `fn` and a list of
  * it's arguments. A good hashing function will discard irrelevant
  * arguments and join the resulting arguments
- * @param {function} cacheresult a predicate function that will be called
+ * @param {function} opts.cacheresult a predicate function that will be called
  * with the result of the function execution. Only when this function returns
  * `true` will the result be cached. The default is that all results are
  * cached.
- * @param {function} cacheerror a predicate function that will be called
+ * @param {function} opts.cacheerror a predicate function that will be called
  * with the error that the function execution throws. Only when this
  * function returns `true` will the error be cached. The default is that
  * errors are never cached, so that each new invocation will be uncached.
  */
-function cache(fn, {
-  hash = (...args) => args.join(),
-  cacheresult = () => true,
-  cacheerror = () => false,
-} = {}) {
+function cache(fn, opts = {}) {
+  const {
+    hash = (...args) => args.join(),
+    cacheresult = () => true,
+    cacheerror = () => false,
+  } = opts;
   return async function cached(...args) {
     const key = hash(fn, ...args);
     if (lru.has(key)) {
